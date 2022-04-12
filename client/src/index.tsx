29c338b62ada9abba5fc6c828a9e686c188f6e45
Kirill Hudokon <k.hudokon@do-it.co>
2022-04-12 20:03:02 +0300

    initialize store

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { Provider } from "react-redux";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { setupStore } from "./store/store";
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
+const store = setupStore();
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
