86b89b338d535be1659e1de03fd47cc44d2377b2
Kirill Hudokon <k.hudokon@do-it.co>
2022-04-12 20:49:29 +0300

    add browser router to index

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -12,7 +13,9 @@ const store = setupStore();
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
